558be52666e17fe140ad04c79699cb69cec28296
Jonathan Doughty <jonathan@mentat.cloud>
2026-04-27 17:14:27 -0400

    chore: remove ephemeral migration scripts

--- src/worker-manager/test_import.py
+++
@@ -1,9 +0,0 @@
-def run_worker():
-    from handlers.pm import handle_pm_dispatcher_worker
-    print(handle_pm_dispatcher_worker())
-
-def helper():
-    return "helper"
-
-if __name__ == '__main__':
-    run_worker()
